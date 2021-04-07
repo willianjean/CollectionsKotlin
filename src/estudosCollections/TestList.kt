dcd0ab9fc3fa61812ca13f95db554541019f19c7
Willian Jean <willianjean@gmail.com>
2021-04-06 22:25:31 -0400

    Alteração do codigo e utilização do sortedBy seguida de um forEache

--- src/estudosCollections/TestList.kt
+++ src/estudosCollections/TestList.kt
@@ -1,8 +1,8 @@
 package estudosCollections
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
+    val joao = Funcionario("João", 2000.0)
+    val pedro = Funcionario("Pedro", 1500.0)
     val maria = Funcionario("Maria", 4000.0)
 
     val funcionarios = listOf(joao, pedro, maria)
@@ -11,6 +11,9 @@ fun main() {
 
     espaco()
     println(funcionarios.find { it.nome == "Maria" })
+
+    espaco()
+    funcionarios.sortedBy { it.salario }.forEach{ println(it) }
 }
 
 data class Funcionario(
