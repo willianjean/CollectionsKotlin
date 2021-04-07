d8423fadc79a851ec5dabd6aec11b761065e84e8
Willian Jean <willianjean@gmail.com>
2021-04-06 21:38:21 -0400

    Mais operações collections/Kotlin

--- src/estudosCollections/TesteOperacoes.kt
+++ src/estudosCollections/TesteOperacoes.kt
@@ -15,4 +15,15 @@ fun main() {
     espaco()
     val salariosMaiorQue2000 = salarios.filter { it > 2000.0 }
     salariosMaiorQue2000.forEach{ println(it) }
+
+    espaco()
+    println(salarios.count{ it in 2000.0..5000.0})
+
+    espaco()
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+    espaco()
+    println(salarios.any{ it == 1000.0 })
+    println(salarios.any{ it == 500.0})
 }
